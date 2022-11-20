b9210df0f7a8e1db0ceced3c178972c7b03711a4
d4tapunk <108387783+d4tapunk@users.noreply.github.com>
2022-11-20 01:19:49 -0800

    Add files via upload
    Project's Steps.
    
    Tableau Visualization Analysis.
    https://public.tableau.com/app/profile/d4tapunk/viz/AbsenteeismPredictionAnalysis/Dashboard1?publish=yes

---
+++ query1.sql
@@ -0,0 +1,25 @@
+use predicted_outputs;
+
+DROP TABLE IF EXISTS predicted_outputs;
+CREATE TABLE predicted_outputs
+(
+	reason_1 BIT NOT NULL,
+    reason_2 BIT NOT NULL,
+    reason_3 BIT NOT NULL,
+    reason_4 BIT NOT NULL,
+    month_value INT NOT NULL,
+    transportation_expense INT NOT NULL,
+    age INT NOT NULL,
+    body_mass_index INT NOT NULL,
+    education BIT NOT NULL,
+    children INT NOT NULL,
+    pets INT NOT NULL,
+    probability FLOAT NOT NULL,
+    prediction BIT NOT NULL
+);
+
+SELECT *
+from
+predicted_outputs;
+
+INSERT_INTO predicted_outputs VALUES
